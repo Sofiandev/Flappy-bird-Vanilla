92aa212c8c0f2eb189139bdf53e60557a11d5068
SofianAcDev <sofian.achbabi@gmail.com>
2023-05-21 19:04:09 +0200

    init

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = "./media/flappy-bird-set.png";
+
+// general settings
+let gamePlaying = false;
+const gravity = 0.5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = canvas.width / 10;
+
+let index = 0,
+  bestScore = 0,
+  currentScore = 0,
+  pipes = [],
+  flight,
+  flyHeight;
+
+const render = () => {
+  index++;
+
+ctx.drawImage(img, 432, Math.floor((index%9)/3) * size[1], ...size, ((canvas.width /2) - size[0]/2), flyHeight, ...size);
+flyHeight = (canvas.height /2)-(size[1]/2)
+
+  window.requestAnimationFrame(render);
+};
+img.onload = render;
